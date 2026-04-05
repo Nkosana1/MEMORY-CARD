abb078ec23ef15250a80e02e1d7dc84792fd3acb
Nkosana1 <nkosanazazu@gmail.com>
2026-04-05 21:07:52 +0900

    Save current App.js changes

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -10,9 +10,53 @@ function App() {
     {id: 1, text: 'Learn React useState', completed: false},
     {id: 2, text: 'Build first project', completed: false},
     {id: 3, text: 'Show chat i did it', completed: true},
-  ])
+  ]);
+
+  //Function to add a new task
+  const addTask = (taskText)=> {
+    if (!taskText.trim()) return; //dont add empty tasks
+
+    const newTask = {
+      id: Date.now(), //simple unique ID
+      text: taskText,
+      completed: false,
+    };
+
+    setTasks([]); // spread old tasks and add new one
+
+
+  };
+//Function to toggle completed status
+
+const toggleTask = (id) => {
+
+  setTasks(tasks.map((task) =>
+    task.id === id? {...task, completed: !task.completed} : task
+)
+  );
+
+};
+
+//Function to delete a task
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !== id));
+
+};
+
+
+
   return (
-    
+      <div className='App'>
+           <h1>My Task Tracker</h1>
+           <AddTask onAdd={addTask}/>
+           <TaskList 
+                tasks={tasks} 
+                onToggle={toggleTask} 
+                onDelete={deleteTask} 
+      />
+
+      </div>
   );
 }
 
